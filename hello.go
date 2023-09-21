87fcda41aeb1a78fd3bbbad25537aa90ec278bbe
kalio007 <kalioprince1@gmail.com>
2023-09-21 06:47:19 +0100

    more on Go

--- hello.go
+++ hello.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"math"
+	"time"
 
 	"rsc.io/quote"
 
@@ -50,4 +51,53 @@ func main() {
 	// k := 3
 	// c, python, java := true, false, "no!"
 
+	var ToBe bool = false
+	var s string
+	fmt.Printf("Type: %T value: %v, %q", ToBe, ToBe, s)
+
+	//you have to be very specific about your types
+	var x, y int = 3, 4
+	var f float64 = math.Sqrt(float64(x*x + y*y))
+	var z uint = uint(f)
+	fmt.Println(x, y, z)
+
+	//in cont declaration we cant use :=
+	const Truth = true
+	fmt.Println("Go rules?", Truth)
+
+	// for loop
+
+	//declare
+	// i := 1
+	// for i <= 3 {
+	// 	fmt.Println(i)
+	// 	i = i + 1
+	// }
+
+	for j := 7; j <= 9; j++ {
+		fmt.Println(j)
+	}
+
+	//if statements
+	if 7%2 == 0 {
+		fmt.Println("7 is even")
+	} else {
+		fmt.Println("7 is odd")
+	}
+
+	//another
+	if num := 7; num < 0 {
+		fmt.Println(num, "is negative")
+	} else if num < 10 {
+		fmt.Println(num, "has 1 digit")
+	}
+
+	// time and switch
+	t := time.Now()
+	switch {
+	case t.Hour() < 12:
+		fmt.Printf("its before noon")
+	default:
+		fmt.Println("Its after noon")
+	}
 }
